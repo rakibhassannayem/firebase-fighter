75bb2fd21f3b271206f6ad662b08bb35840b9915
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-21 12:31:20 +0600

    create contextAPI

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/routes";
 import { ToastContainer } from "react-toastify";
+import AuthProvider from "./context/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <ToastContainer autoClose={1000}/>
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer autoClose={1000} />
+    </AuthProvider>
   </StrictMode>
 );
